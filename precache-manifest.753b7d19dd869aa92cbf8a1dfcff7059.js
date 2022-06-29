ddaa6aca934312a2bf26e449c0f05122bf6d5f62
Mrinal <mrinal.sharma2020@vitstudent.ac.in>
2022-06-29 21:58:40 +0530

    Updates

---
+++ precache-manifest.753b7d19dd869aa92cbf8a1dfcff7059.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1c8dce919bba609ce236786916082f",
+    "url": "/keeper-app-react/index.html"
+  },
+  {
+    "revision": "91dcddfb80aa5ef12447",
+    "url": "/keeper-app-react/static/js/2.32b9d141.chunk.js"
+  },
+  {
+    "revision": "af2e9fdc49a4f2fc368c",
+    "url": "/keeper-app-react/static/js/main.779017da.chunk.js"
+  },
+  {
+    "revision": "925a570a761bd77c288c",
+    "url": "/keeper-app-react/static/js/runtime-main.874ed7da.js"
+  }
+]);
